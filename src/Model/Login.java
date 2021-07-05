f4c33ba4e282398b07f7e20953843924785f205e
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-07-05 18:13:20 -0300

    Faz Classe de Login

--- src/Model/Login.java
+++ src/Model/Login.java
@@ -1,4 +1,31 @@
 package Model;
 
 public class Login {
+    private User usuario;
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
+
+    public String autenticaUsuario(User usuario){
+        // ........
+
+        return "Usuario autenticado com sucesso";
+    }
+
+    public User exibeDadosLogado(User usuario){
+        // ........
+
+        return usuario;
+    }
+
+    public String encerraAutenticacao() {
+        // ........
+
+        return "Autenticacao encerrada";
+    }
 }
